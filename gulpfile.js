d6a327482703c570e02160f6d9a7c9026d7cff9b
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-09-25 09:46:03 +0500

    Добавляет задачу fonts для gulp

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,11 @@ const styles = () => {
     .pipe(browserSync.stream());
 };
 
+const fonts = () => {
+  return gulp.src('./src/fonts/**/*.{woff,woff2}')
+  .pipe(gulp.dest('./build/fonts'));
+};
+
 const scripts = () => {
   return gulp.src(jsFiles)
     .pipe(concat('index.js'))
@@ -46,139 +51,20 @@ const watch = () => {
   browserSync.init({
     server: {
       baseDir: './'
-    },
-    tunnel: true
+    }
   });
   gulp.watch('./src/css/**/*.css', styles);
   gulp.watch('./src/js/**/*.js', scripts);
-  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('./src/*.html', browserSync.reload);
 };
 
 gulp.task('styles', styles);
+gulp.task('fonts', fonts);
 gulp.task('scripts', scripts);
 gulp.task('watch', watch);
 gulp.task('clean', clean);
 
 gulp.task('build', gulp.series(clean,
-                                gulp.parallel(styles, scripts)));
+                                gulp.parallel(styles, fonts, scripts)));
 
 gulp.task('dev', gulp.series('build', watch));
-
-// var less = require("gulp-less");
-// var plumber = require("gulp-plumber");
-// var postcss = require("gulp-postcss");
-// var autoprefixer = require("autoprefixer");
-// var minify = require("gulp-csso");
-// var rename = require("gulp-rename");
-// var imagemin = require("gulp-imagemin");
-// var webp = require("gulp-webp");
-// var svgstore = require("gulp-svgstore");
-// var posthtml = require("gulp-posthtml");
-// var include = require("posthtml-include");
-// var del = require("del");
-// var htmlmin = require("gulp-htmlmin");
-// var uglify = require("gulp-uglify");
-// var server = require("browser-sync").create();
-// var run = require("run-sequence");
-
-// gulp.task("copy", function() {
-//   return gulp.src([
-//     "source/fonts/**/*.{woff,woff2}",
-// 	"source/img/**",
-// 	"source/js/**"
-//   ], {
-// 	  base: "source"
-//   })
-//   .pipe(gulp.dest("build"));
-// });
-
-// gulp.task("clean", function() {
-//   return del("build");
-// });
-
-// gulp.task("style", function() {
-//   gulp.src("source/less/style.less")
-//     .pipe(plumber())
-//     .pipe(less())
-//     .pipe(postcss([
-//       autoprefixer()
-//     ]))
-//     .pipe(gulp.dest("build/css"))
-// 	.pipe(minify())
-// 	.pipe(rename("style.min.css"))
-// 	.pipe(gulp.dest("build/css"))
-//     .pipe(server.stream());
-// });
-
-// gulp.task("jsmin", function () {
-//   return gulp.src("build/js/*.js")
-//     .pipe(uglify())
-//     .pipe(rename({suffix: ".min"}))
-//     .pipe(gulp.dest("build/js"));
-// });
-
-// gulp.task("htmlmin", function() {
-//   return gulp.src("build/*.html")
-//     .pipe(htmlmin({collapseWhitespace: true}))
-//     .pipe(gulp.dest("build"));
-// });
-
-// gulp.task("images", function() {
-//   return gulp.src("build/img/**/*.{png,jpg,svg}")
-//     .pipe(imagemin([
-//       imagemin.optipng({optimizationLevel: 3}),
-//       imagemin.jpegtran({progressive: true}),
-//       imagemin.svgo()
-//     ]))
-//     .pipe(gulp.dest("build/img"));
-// });
-
-// gulp.task("webp", function() {
-//   return gulp.src("build/img/**/*.{png,jpg}")
-// 	.pipe(webp({quality: 90}))
-// 	.pipe(gulp.dest("build/img"));
-// });
-
-// gulp.task("sprite", function() {
-//   return gulp.src("source/img/sprite-*.svg")
-//     .pipe(svgstore({
-// 		inlineSvg: true
-// 	}))
-//     .pipe(rename("sprite.svg"))
-//     .pipe(gulp.dest("build/img"));
-// });
-
-// gulp.task("html", function() {
-//   return gulp.src("source/*.html")
-//     .pipe(posthtml([
-// 	  include()
-// 	  ]))
-//     .pipe(gulp.dest("build"));
-// });
-
-// gulp.task("serve", function() {
-//   server.init({
-//     server: "build/",
-//     notify: false,
-//     open: true,
-//     cors: true,
-//     ui: false
-//   });
-
-//   gulp.watch("source/less/**/*.less", ["style"]);
-//   gulp.watch("source/*.html", ["html"]);
-// });
-
-// gulp.task("build", function(done) {
-//   run(
-//     "clean",
-//     "copy",
-//     "style",
-//     "jsmin",
-//     "images",
-//     "webp",
-//     "sprite",
-//     "html",
-//     "htmlmin",
-//     done);
-// });
